c922f88e19f19ca0416caa9523d8142d1154804e
Stefano199605 <kevin.saldana1996@gmail.com>
2022-04-05 12:50:58 -0500

    presentar visualizacion de form1

--- sistemaAlmacen/sistemaAlmacen/Form1.Designer.cs
+++ sistemaAlmacen/sistemaAlmacen/Form1.Designer.cs
@@ -30,17 +30,17 @@ namespace sistemaAlmacen
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button3 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -52,6 +52,16 @@ namespace sistemaAlmacen
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(261, 262);
             this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::sistemaAlmacen.Properties.Resources.Serpost;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(231, 87);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // textBox1
             // 
@@ -120,16 +130,6 @@ namespace sistemaAlmacen
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::sistemaAlmacen.Properties.Resources.Serpost;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 91);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(231, 87);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.SteelBlue;
@@ -137,7 +137,7 @@ namespace sistemaAlmacen
             this.button1.ForeColor = System.Drawing.SystemColors.Control;
             this.button1.Image = global::sistemaAlmacen.Properties.Resources.icons8_close_25;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(552, 11);
+            this.button1.Location = new System.Drawing.Point(595, 11);
             this.button1.Name = "button1";
             this.button1.Padding = new System.Windows.Forms.Padding(3, 0, 0, 0);
             this.button1.Size = new System.Drawing.Size(39, 36);
@@ -165,8 +165,8 @@ namespace sistemaAlmacen
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
