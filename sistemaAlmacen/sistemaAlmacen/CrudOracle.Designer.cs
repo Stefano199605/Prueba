28baa0175e390765558f7d559ad13b9e5fb4c698
jjvfernandez <102260465+jjvfernandez@users.noreply.github.com>
2022-04-05 19:55:23 -0500

    aa

--- sistemaAlmacen/sistemaAlmacen/CrudOracle.Designer.cs
+++ sistemaAlmacen/sistemaAlmacen/CrudOracle.Designer.cs
@@ -190,6 +190,7 @@ namespace sistemaAlmacen
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
